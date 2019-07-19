059b01ca59a14a118e835796751431e44f78c61d
rinoy <rinoy@promactinfo.com>
2019-07-19 14:48:24 +0530

    Add project files.

---
+++ Assignment1/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Employee
+    {
+        private int Id;
+        private string Name, DepartmentName;
+        public Employee(int Id, string Name, string DepartmentName)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.DepartmentName = DepartmentName;
+        }
+        public string GetId()
+        {
+            functionCalled("GetID");
+            return Id.ToString();
+        }
+        public string GetName()
+        {
+            functionCalled("GetName");
+            return Name;
+        }
+        public string GetDepartmentName()
+        {
+            functionCalled("GetDepartmentName");
+            return DepartmentName;
+        }
+
+        /*
+        I was not sure how to interpret this requirement "Create 3 overloaded methods 
+        which allows updating any of the Employee property (Id, Name, DepartmentName)" because the updation of both 
+        Name and DepartmentName just requires one string. I added an int flag to differentiate the two.
+        */
+        public void Update(int id)
+        {
+            this.Id = id;
+        }
+
+        public void Update(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Update(string dept, int flag)
+        {
+            this.DepartmentName = dept;
+        }
+
+        public FunctionCalledDelegate functionCalled;
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             
+
+            Console.WriteLine("Enter Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Deptartment:");
+            string dept = Console.ReadLine();
+
+            Employee emp = new Employee(id, name, dept);
+            emp.functionCalled = new FunctionCalledDelegate(onFunctionCall);
+
+            Console.WriteLine("ID: " + emp.GetId());
+            Console.WriteLine("Name: " + emp.GetName());
+            Console.WriteLine("Department: " + emp.GetDepartmentName());
+
+            Console.WriteLine("***Update Calls***");
+            emp.Update(6);//calls first method
+            emp.Update("NewName");//calls second method
+            emp.Update("NewDeptName",1);//calls third method, the integer '1' can be replaced with any integer as it is not used
+
+            Console.WriteLine("ID: " + emp.GetId());
+            Console.WriteLine("Name: " + emp.GetName());
+            Console.WriteLine("Department: " + emp.GetDepartmentName());
+
+            Console.ReadKey();
+        }
+
+        static void onFunctionCall(string x)
+        {
+            Console.WriteLine(x+" called!");
+        }
+    }
+}
